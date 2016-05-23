0c6429ccd91dc16ab6b35225bec07c5822c292f8
Ray-mech <7secworks@gmail.com>
2016-05-23 22:30:26 +0900

    Update straight.py

--- straight.py
+++ straight.py
@@ -19,15 +19,20 @@ def DrawNum(myWay,setWay,motor):
     cv2.waitKey(1)
     
 #移動の軌跡を描画する関数--------------------------------------------------------
-def DrawTrack(myWay,frame):
+def DrawTrack(myWay,frame,motor,drawFlg):
 #    print(myWay)    
-    plt.plot(frame, myWay, color="k",marker="o",markersize=10)
+    plt.plot(frame, myWay, color="k",marker="o",markersize=10,label="distance")
+    plt.plot(frame, motor, color="b",marker="o",markersize=10,label="power")
+    
+    if drawFlg == 0:
+        plt.legend(bbox_to_anchor=(0.28, 1), loc='bottom left', borderaxespad=0)
+
     plt.pause(0.01)
     
 #速度を計算する関数--------------------------------------------------------
 def Velocity(myWay,drawProb,preMyWay):
         velocity = (myWay - preMyWay)/drawProb * 100
-        print(velocity, "cm/frame")
+#        print(velocity, "cm/frame")
         preMyWay = myWay
         return velocity,preMyWay
 
@@ -40,14 +45,19 @@ def GoStraight(setWay=5):
     preMyWay = 0
     myWay = 0
     gainMotor = 0.001
-    kp = 0.3
+    motorPow = 1.0
+    drawFlg = 0
     #始動
     motor = 0
     while 1:
         frame += 1
     #  ゆっくり始動               #
-        if motor < 1:
-            motor +=gainMotor
+        print("Going straight")
+        if myWay < 0.5:
+            if motor < 1:
+                motor += gainMotor
+        else:
+            motor = motorPow
     #                        #
         ccm = 0.102 * 3.14159 #[m] 円周の長さ
         pulse += motor*2 #Get Pulse
@@ -55,35 +65,37 @@ def GoStraight(setWay=5):
         myWay = ccm * angle/360
         restWay = setWay - myWay
 
-
-
     #各種描画関数を任意のフレーム毎に呼び出す
-        drawProb = 100 #[frame/１点] 点をプロットする頻度の逆数
+        drawProb = 120 #[frame/１点] 点をプロットする頻度の逆数
         drawOrNot = frame % drawProb
-
         if drawOrNot == 0:
             velocity,preMyWay = Velocity(myWay,drawProb,preMyWay)
-            DrawTrack(myWay,frame)
+            DrawTrack(myWay,frame,motor,drawFlg)
+            drawFlg = 1
             DrawNum(myWay,setWay,motor)
-
-        gainMotor = kp * restWay
+            
     #ループ処理(条件分岐)
+        if restWay <= 0:
+            motor = 0
+            print("complete")
+            DrawTrack(myWay,frame,motor,drawFlg)
+            DrawNum(myWay,setWay,motor)
+            break
+        
+        if motor > 0 and restWay <= 0.5:
+#            print("減速中")
+            motorPow -=  0.00022*(1/(restWay+0.1))
+        elif motor < 0:
+            motorPow += 0.001
 
-        if myWay >= setWay - 0.5:
-            if myWay >= setWay - 0.05:
-                motor = 0
-                print("stop")
-                print(myWay)
-                break
-            elif motor > 0:
-                #print("減速中")
-#                motor -= 0.0010001 *1/restWay
-
-            else:
-                motor +=0.0015
-
+def ClrWindow():
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    plt.clf()
 
 #メイン関数---------------------------------------------------------------
 if __name__ == "__main__":
+    GoStraight(2)
+    ClrWindow()
     GoStraight(5)
-    cv2.waitKey(0)
+    ClrWindow()
